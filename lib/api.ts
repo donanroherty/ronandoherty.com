129743492caf0267c650be14bb8a1d3faf7ab328
Ronan Doherty <donanroherty@gmail.com>
2021-02-19 09:53:20 +0000

    Updated API to use PostType partial

--- lib/api.ts
+++ lib/api.ts
@@ -19,31 +19,31 @@ export function getPostBySlug(file: string, fields: string[]) {
         [key: string]: string
     }
 
-    const items: Items = {}
+    let postData: Partial<PostType> = {}
 
     fields.forEach((field) => {
         if (field === 'slug') {
             const title: string = data['title']
             const slug: string = title.replace(/[^A-Za-z0-9]+/g, '-').toLowerCase()
-            items[field] = slug
+            postData.slug = slug
         }
         if (field === 'content') {
-            items[field] = content
+            postData.content = content
         }
         if (data[field]) {
-            items[field] = data[field]
+            postData[field as keyof PostType] = data[field]
         }
     })
 
-    return items
+    return postData
 }
 
 export function getAllPosts(fields: Array<keyof PostType>) {
     const filenames = getPostSlugs()
 
-    const posts = filenames
+    let posts = filenames
         .map(file => getPostBySlug(file, fields))
-        .sort((a, b) => a.date > b.date ? -1 : 1)
+        .sort((a, b) => (a.date && b.date) ? (a.date > b.date ? -1 : 1) : 0)
 
     return posts
 }
