c0aa7d82849a4063db5e34cc6d566060b3a22a41
Ronan Doherty <donanroherty@gmail.com>
2021-06-18 23:39:43 +0100

    Added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,12 +6,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="icon"
-            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦆</text></svg>"
-          />
+          <link rel="icon" href="assets/RD.svg" />
         </Head>
-        <body className="bg-white dark:bg-gray-800 transition-colors duration-500">
+        <body className="transition-colors duration-500 bg-white dark:bg-gray-800">
           <Main />
           <NextScript />
         </body>
