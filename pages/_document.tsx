4a3d3eb46f913bd1a354ef74737e57056d954f93
Ronan Doherty <donanroherty@gmail.com>
2021-06-19 13:35:06 +0100

    Google analytics setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,20 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="assets/RD.svg" />
+          <link rel="icon" href="assets/RD-circle.svg" />
+
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"></script>
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              `,
+            }}
+          />
         </Head>
         <body className="transition-colors duration-500 bg-white dark:bg-gray-800">
           <Main />
